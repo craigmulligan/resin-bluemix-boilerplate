eee0a0a071c428580999ea4aec2490bc58f1e05e
Craig Mulligan <craig@firingsuad.co.za>
2015-07-03 14:44:10 +0100

    pep'd

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
 # config
 import os
 # resin config
-JWT = os.getenv('JWT') #app envar - javascript web token, used for auth with resin.io api
-resin_device_uuid = os.getenv('RESIN_DEVICE_UUID') #app envar - 62 char unique device id provided by resin.io
+# app envar - javascript web token, used for auth with resin.io api
+JWT = os.getenv('JWT')
+# app envar - 62 char unique device id provided by resin.io
+resin_device_uuid = os.getenv('RESIN_DEVICE_UUID')
 
-#bluemix API config 
-bluemix_api_key = os.getenv('API_KEY') #app envar 
-bluemix_api_token = os.getenv('API_TOKEN') #app envar
+# bluemix API config
+bluemix_api_key = os.getenv('API_KEY')  # app envar
+bluemix_api_token = os.getenv('API_TOKEN')  # app envar
 
 # bluemix Device config
-organization = os.getenv('ORGANIZATION') #app envar
-device_type = os.getenv('DEVICE_TYPE') #app envar - categorizes device on bluemix
-auth_method = os.getenv('AUTH_METHOD', "token") #app envar
+organization = os.getenv('ORGANIZATION')  # app envar
+# app envar - categorizes device on bluemix
+device_type = os.getenv('DEVICE_TYPE')
+auth_method = os.getenv('AUTH_METHOD', "token")  # app envar
 
-device_id = os.getenv('DEVICE_ID') # device envar set on devices first boot
-auth_token = os.getenv('AUTH_TOKEN') # device envar set on devices first boot
+device_id = os.getenv('DEVICE_ID')  # device envar set on devices first boot
+auth_token = os.getenv('AUTH_TOKEN')  # device envar set on devices first boot
--- publish.py
+++ publish.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-import time, random, json
+import time
+import random
+import json
 import ibmiotf.device
 from config import organization, device_type, auth_method, device_id, auth_token
 
@@ -7,31 +9,30 @@ client = None
 
 # bluemix connect
 try:
-  options = {
-    "org": organization,
-    "type": device_type,
-    "id": device_id,
-    "auth-method": auth_method,
-    "auth-token": auth_token
-  }
-  client = ibmiotf.device.Client(options)
-  client.connect()
-  print "Device connect to IBM IoT dashboard - https://" + organization + ".internetofthings.ibmcloud.com/dashboard/#/devices"
+    options = {
+        "org": organization,
+        "type": device_type,
+        "id": device_id,
+        "auth-method": auth_method,
+        "auth-token": auth_token
+    }
+    client = ibmiotf.device.Client(options)
+    client.connect()
+    print "Device connect to IBM IoT dashboard - https://" + organization + ".internetofthings.ibmcloud.com/dashboard/#/devices"
 
-except ibmiotf.ConnectionException  as e:
+except ibmiotf.ConnectionException as e:
     print e
-
 
 
 # bluemix publish event
 while True:
     try:
         # Let's make some random values for our sensors
-        sensor_1 = random.random()
+        sensor_1 = random.random()å
         sensor_2 = random.random()
 
         # prefix data with 'd' to distinguish a device published event.
-        myData = { "d": {"Sensor one": sensor_1, "Sensor Two": sensor_2}}
+        myData = {"d": {"Sensor one": sensor_1, "Sensor Two": sensor_2}}
         # publish event named status
         client.publishEvent(event="status", msgFormat="json", data=myData)
         time.sleep(1)
